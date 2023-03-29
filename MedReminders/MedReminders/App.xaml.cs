4ac94a638696846329561ef5ac593b231ab94fd0
jingenito <jingenito1530@gmail.com>
2023-03-29 17:19:07 -0600

    added the current state of the app

---
+++ MedReminders/MedReminders/App.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MedReminders
+{
+    public partial class App : Application
+    {
+        public static class AppKeys
+        {
+            public static string CheckBoxCheckedStateKey = "CheckBoxCheckedStateKey";
+            public static string CheckedStateTimeStamp = "CheckedStateTimeStamp";
+        }
+
+        public bool CheckBoxCheckedState
+        {
+            get
+            {
+                if (CurrentApp.Properties.ContainsKey(AppKeys.CheckBoxCheckedStateKey))
+                    return (bool)CurrentApp.Properties[AppKeys.CheckBoxCheckedStateKey];
+                return false;
+            }
+            set
+            {
+                CurrentApp.Properties[AppKeys.CheckBoxCheckedStateKey] = value;
+            }
+        }
+
+        public DateTime CheckedStateTimeStamp
+        {
+            get
+            {
+                if (CurrentApp.Properties.ContainsKey(AppKeys.CheckedStateTimeStamp))
+                    return (DateTime)CurrentApp.Properties[AppKeys.CheckedStateTimeStamp];
+                return DateTime.MinValue;
+            }
+            set
+            {
+                CurrentApp.Properties[AppKeys.CheckedStateTimeStamp] = value;
+            }
+        }
+
+        /// <summary>
+        /// Call this method to retrieve the current application instance.
+        /// </summary>
+        public static App CurrentApp
+        {
+            get
+            {
+                if (Application.Current != null)
+                    return (App)Application.Current;
+                return null;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected async override void OnSleep()
+        {
+            await CurrentApp.SavePropertiesAsync();
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        /// <summary>
+        /// Call this method to change the state of the Checkbox CheckedState, since this method will always 
+        /// handle updating the timestamp properly as well.
+        /// </summary>
+        /// <param name="newState"></param>
+        public void UpdateCheckedState(bool newState)
+        {
+            if (CurrentApp.CheckBoxCheckedState != newState)
+            {
+                CurrentApp.CheckBoxCheckedState = newState;
+                if (newState)
+                {
+                    CurrentApp.CheckedStateTimeStamp = DateTime.Now;
+                }
+            }
+        }
+    }
+}
---
+++ MedReminders/MedReminders/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MedReminders
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool _checkedState;
+        public bool CheckedState
+        {
+            get { return _checkedState; }
+            set
+            {
+                _checkedState = value;
+                OnPropertyChanged("CheckedState");
+            }
+        }
+
+        private DateTime _previousTimeStamp;
+        public DateTime PreviousTimeStamp
+        {
+            get { return _previousTimeStamp; }
+            set
+            {
+                _previousTimeStamp = value;
+                OnPropertyChanged("PreviousTimeStamp");
+                OnPropertyChanged("FormattedPreviousTimeStamp");
+            }
+        }
+
+        public string FormattedPreviousTimeStamp
+        {
+            get
+            {
+                if (_previousTimeStamp == DateTime.MinValue)
+                {
+                    return "N/A";
+                }
+                else
+                {
+                    return _previousTimeStamp.ToString("MM/dd/yyyy HH:mm");
+                }
+            }
+        }
+
+        private string _formattedRefreshTimeStamp;
+        public string FormattedRefreshTimeStamp
+        {
+            get { return _formattedRefreshTimeStamp; }
+            set
+            {
+                _formattedRefreshTimeStamp = value;
+                OnPropertyChanged("FormattedRefreshTimeStamp");
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        protected override void OnAppearing()
+        {
+            RefreshPage();
+        }
+
+        public void RefreshPage()
+        {
+            var timeStamp = App.CurrentApp.CheckedStateTimeStamp;
+            var nextTimeStamp = timeStamp.AddDays(1);
+            var now = DateTime.Now;
+            
+            PreviousTimeStamp = timeStamp;
+            FormattedRefreshTimeStamp = now.ToString("MM/dd/yyyy HH:mm");
+
+            if (now >= timeStamp && now < nextTimeStamp)
+            {
+                //inside this half open interval we want the checked state to persist
+                CheckedState = App.CurrentApp.CheckBoxCheckedState;
+            }
+            else if (now >= nextTimeStamp)
+            {
+                //inside this closed ray we want the checked state to be false
+                CheckedState = false;
+                App.CurrentApp.UpdateCheckedState(false);
+            }
+        }
+
+        private async void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            var newState = e.Value;
+            if (!newState)
+            {
+                bool response = await DisplayAlert("Alert",
+                    "Are you sure you want to uncheck the checked state?",
+                    "Yes",
+                    "No");
+                if (!response)
+                {
+                    CheckedState = true; //suppress the toggle action
+                    return;
+                }
+            }
+            App.CurrentApp.UpdateCheckedState(newState);
+            RefreshPage();
+        }
+
+        private async void buttSave_Clicked(object sender, EventArgs e)
+        {
+            var msg = "";
+            try
+            {
+                await App.CurrentApp.SavePropertiesAsync();
+                msg = "Timestamp has successfully been saved.";
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+            await DisplayAlert("Save Receipt", msg, "OK");
+        }
+    }
+}
